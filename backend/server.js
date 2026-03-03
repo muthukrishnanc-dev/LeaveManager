520ac7cbf4914221ccade58de70ed690a9702671
muhtukrishnanc-dev <muthukrishnan.c.code@gmail.com>
2026-03-03 18:42:40 +0530

    add environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
